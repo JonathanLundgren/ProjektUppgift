9d1f620cc8f26490cab20fd7d96f6c0f0cfa44d9
Etnojius <81381496+Etnojius@users.noreply.github.com>
2024-04-11 09:45:54 +0200

    3D start

--- ProjektUppgift/Form1.Designer.cs
+++ ProjektUppgift/Form1.Designer.cs
@@ -30,22 +30,35 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // timer1
             // 
             this.timer1.Enabled = true;
-            this.timer1.Interval = 200;
+            this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1184, 761);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -53,6 +66,7 @@
         #endregion
 
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- ProjektUppgift/Form1.cs
+++ ProjektUppgift/Form1.cs
@@ -13,56 +13,93 @@ namespace ProjektUppgift
 {
     public partial class Form1 : Form
     {
-        const int size = 75;
-        Picture[,] pictures = new Picture[size, size];
-        Random rand = new Random();
+        const int width = 1200;
+        const int height = 800;
+        const int resolution = 4;
+        const int fovHorizontal = 90;
+        const int fovVertical = 90;
+        readonly int newWidth = width / resolution;
+        readonly int newHeight = height / resolution;
+        Pixel[,] pixels = new Pixel[width / resolution, height / resolution];
+        int[] testRoom = new int[25] { 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1 };
+        double angle = 0;
+        double positionX = 0;
+        double positionY = 0;
         public Form1()
         {
             InitializeComponent();
-
-            for (int i = 0; i < size; i++) 
+            pictureBox1.ClientSize = new Size(width, height);
+            this.Size = new Size(width, height);
+            for (int i = 0; i < newWidth; i++) 
             {
-                for (int j = 0; j < size; j++) 
+                for (int j = 0; j < newHeight; j++) 
                 {
-                    pictures[i, j] = new Picture(i, j);
-                    Controls.Add(pictures[i, j].pictureBox);
+                    pixels[i, j] = new Pixel(i, j, fovHorizontal * ((newWidth / 2) - i) / newWidth, fovVertical * ((newHeight / 2) - i) / newHeight);
                 }
             }
+        }
+
+        private List<Face> GenerateRoom()
+        {
+            List<Face> room = new List<Face>();
+
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            for (int i = 0; i < size; i++)
+            UpdateImage();
+        }
+
+        public void UpdateImage()
+        {
+            Bitmap bmp = new Bitmap(newWidth, newHeight);
+            for (int i = 0; i < newWidth; i++)
             {
-                for (int j = 0; j < size; j++)
+                for (int j = 0; j < newHeight; j++)
                 {
-                    pictures[i,j].pictureBox.BackColor = Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256));
+                    Color color = pixels[i, j].GetColor();
+                    bmp.SetPixel(i, j, color);
                 }
             }
+            pictureBox1.Image = bmp;
         }
     }
 
-    public class Picture
+    public class Pixel
     {
-        const int size = 10;
         int posX;
         int posY;
+        double angleHorizontal;
+        double angleVertical;
         public Label pictureBox;
         Random rand;
 
-        public Picture(int x, int y) 
+        public Pixel(int x, int y, double angleHorizontal, double angleVertical) 
         {
             posX = x;
             posY = y;
-            pictureBox = new Label();
-            pictureBox.Location = new Point(posX*size, posY*size);
-            pictureBox.Size = new Size(size, size);
+            this.angleHorizontal = angleHorizontal;
+            this.angleVertical = angleVertical;
             rand = new Random(posX + posY * 1000);
         }
 
-        public void ColorChange()
+        public Color GetColor()
         {
-            pictureBox.BackColor = Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256));
+             return Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256));
+        }
+    }
+
+    public class Face
+    {
+        public bool isDirectionX;
+        public double leftBound;
+        public double rightBound;
+
+        Face(bool isDirectionX, double leftBound, double rightBound)
+        {
+            this.isDirectionX = isDirectionX;
+            this.leftBound = leftBound;
+            this.rightBound = rightBound;
         }
     }
 }
